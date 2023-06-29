626b54e4adbcf9dc78baf8c3b5105aba4115ec99
Dzmitry Shauchonak <dzmitry_shauchonak@epam.com>
2023-06-29 22:15:02 +0200

    feat: add API lambda

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,14 +3,14 @@ import { Config } from './config.interface';
 export const environment: Config = {
   production: true,
   apiEndpoints: {
-    product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    product: 'https://8eknvsfgm3.execute-api.eu-central-1.amazonaws.com/prod',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
-    product: false,
+    product: true,
     order: false,
     import: false,
     bff: false,
